8a6eb622fcb8f2837fbabee9ca492f1a587c34b8
James Grider <james.grider@faa.gov>
2021-05-12 09:51:23 -0500

    Reformat code with black

--- src/mags_hypermodern_python/console.py
+++ src/mags_hypermodern_python/console.py
@@ -9,11 +9,11 @@ from . import __version__, wikipedia
 
 @click.command()
 @click.option(
-    '--language',
-    '-l',
-    default='en',
-    help='Language edition of Wikipedia',
-    metavar='LANG',
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
     show_default=True,
 )
 @click.version_option(version=__version__)
@@ -21,8 +21,8 @@ def main(language):
     """The hypermodern Python project."""
     data = wikipedia.random_page(language=language)
 
-    title = data['title']
-    extract = data['extract']
+    title = data["title"]
+    extract = data["extract"]
 
-    click.secho(title, fg='green')
+    click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
