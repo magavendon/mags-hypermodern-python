9fddfc7dbbde1a404d655738588a12c5932ea2ef
James Grider <james.grider@faa.gov>
2021-05-13 14:43:29 -0500

    Add preliminary dataclass usage for wikipedia page

--- src/mags_hypermodern_python/wikipedia.py
+++ src/mags_hypermodern_python/wikipedia.py
@@ -1,13 +1,19 @@
-from typing import Any
+from dataclasses import dataclass
 
 import click
 import requests
 
 
+@dataclass
+class Page:
+    title: str
+    extract: str
+
+
 API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language: str = "en") -> Any:
+def random_page(language: str = "en") -> Page:
     url = API_URL.format(language=language)
 
     try:
