34d34ba6745f84266610dfdc506065c3df715699
James Grider <james.grider@faa.gov>
2021-05-18 07:17:05 -0500

    Add xdoctest via poetry

--- src/mags_hypermodern_python/wikipedia.py
+++ src/mags_hypermodern_python/wikipedia.py
@@ -21,7 +21,7 @@ class Page:
 
 
 API_URL: str = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
-schema = desert.schema(Page, meta={"unkown": marshmallow.EXCLUDE})
+schema = desert.schema(Page, meta={"unknown": marshmallow.EXCLUDE})
 
 
 def random_page(language: str = "en") -> Page:
@@ -39,6 +39,12 @@ def random_page(language: str = "en") -> Page:
     Raises:
         ClickException: The HTTP request failed or the HTTP response contained an
             invalid body.
+
+    Example:
+        >>> from mags_hypermodern_python import wikipedia
+        >>> page = wikipedia.random_page(language="en")
+        >>> bool(page.title)
+        True
     """
     url = API_URL.format(language=language)
 
