70abadb88e37f36ce0eeefeef132f4b42ae4a8b6
James Grider <james.grider@faa.gov>
2021-05-18 08:06:04 -0500

    Activate sphinx addons

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "mags-hypermodern-python"
 author = "James Grider"
 copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napolean",
+    "sphinx_autodoc_typehints",
+]
